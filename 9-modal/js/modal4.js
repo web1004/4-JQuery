c6cf437732ce03174e0a0022033c8da2a40afa77
web1004 <gangsugi@gmail.com>
2025-04-04 16:42:35 +0900

    25.04.04 추가수정

--- 9-modal/js/modal4.js
+++ 9-modal/js/modal4.js
@@ -3,39 +3,39 @@ $(document).ready(function(){
   //각 목록을 클릭했을때.....
   $(".item_list>li").click(function(){
 
-    g_pop=$(this).index();
+    pageNum =$(this).index();
 
-    $(".g_page span:nth-child(1)").text(g_pop+1);  //오른쪽 상단 페이지번호
+    $(".page-num span:nth-child(1)").text(pageNum+1);  //오른쪽 상단 페이지번호
     $("html").css({"overflow-y":"hidden"}); //기존 html 스크롤 숨기기
-    $(".modal-content>li").eq(g_pop).fadeIn();  //각 목록의 내용
+    $(".modal-content>li").eq(pageNum).stop().fadeIn();  //각 목록의 내용
     $(".modal").stop().fadeIn(); //검정배경
 
   });
 
   //이전다음버튼....
   $(".pre").click(function(){
-    if(g_pop>0){
-      $(".modal-content>li").eq(g_pop).stop().fadeOut(); 
-      g_pop--;
-      $(".g_page span:nth-child(1)").text(g_pop+1);
-			$(".modal-content>li").eq(g_pop).stop().fadeIn();
+    if(pageNum>0){
+      $(".modal-content>li").eq(pageNum).stop().hide(); 
+      pageNum--;
+      $(".page-num span:nth-child(1)").text(pageNum+1);
+			$(".modal-content>li").eq(pageNum).stop().fadeIn();
     };
   });
 
   $(".next").click(function(){
-    if(g_pop<14){
-      $(".modal-content>li").eq(g_pop).stop().fadeOut(); 
-      g_pop++;
-      $(".g_page span:nth-child(1)").text(g_pop+1);
-			$(".modal-content>li").eq(g_pop).stop().show();
+    if(pageNum<9){
+      $(".modal-content>li").eq(pageNum).stop().hide(); 
+      pageNum++;
+      $(".page-num span:nth-child(1)").text(pageNum+1);
+			$(".modal-content>li").eq(pageNum).stop().fadeIn();
     };
   });
 
    //닫기버튼.....
-  $(".close").click(function(){
+  $(".close, .modal").click(function(){
     $("html").css({"overflow-y":"scroll"}); 
     $(".modal").stop().fadeOut();
-		$(".modal-content>li").stop().hide();
+		$(".modal-content>li").stop().fadeOut();
   });
 
 });
