269dc9f7f26daec8202fd4916dff53668a89845e
web1004 <gangsugi@gmail.com>
2025-09-11 12:48:01 +0900

    25.09.11수정

--- 8-sitemap/js/sitemap1.js
+++ 8-sitemap/js/sitemap1.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     if(site){
       $(this).find("span").html("<i class='fa fa-chevron-up'></i>");
       $(".footer_sitemap").css({"height":"240px","border-bottom":"1px solid #c3c3c3"});
-      $(".sitemap").fadeIn(); 
+      $(".sitemap").fadeIn();
       site=false
     }else{
       $(this).find("span").html("<i class='fa fa-chevron-down'></i>");
